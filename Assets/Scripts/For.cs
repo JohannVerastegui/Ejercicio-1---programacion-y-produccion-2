7f308fbb0aef9aec6f5214e7bbf9383d7b9406c5
JohannVerastegui <joanvemu2002@gmail.com>
2023-08-31 21:41:39 -0500

    Ejercicio 3- For

---
+++ Assets/Scripts/For.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class For : MonoBehaviour
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
